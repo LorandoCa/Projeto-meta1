0116cf078ebdf71a20379389ab217f6f80f6c441
Pedro Ferreira <pedrobf@student.dei.uc.pt>
2025-10-23 12:09:33 +0100

    fix: Alteração do tipo de parametro

--- Projeto-meta1/StorageBarrelInterface.java
+++ Projeto-meta1/StorageBarrelInterface.java
@@ -5,7 +5,7 @@ import java.rmi.RemoteException;
 
 public interface StorageBarrelInterface extends Remote { 
 
-    public void addWordToStructure(String word, String url) throws RemoteException;
+    public void addWordToStructure(Set<String> word, String url) throws RemoteException;
     public List<String> returnSearchResult(List<String> queryWords) throws RemoteException;
     public void addLinks(String fromUrl, Set<String> toUrls) throws RemoteException;
 }
